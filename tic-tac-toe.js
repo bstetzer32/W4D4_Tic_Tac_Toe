52011d8b21930a89f742dd45e503b485cb4c4b74
Ben Stetzer <benstetzer@gmail.com>
2021-02-05 16:11:11 -0500

    new game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,48 +3,55 @@ let currentPlayer = "x"
 let gameStatus = "";
 
 let gameStatusCheck = () => {
+
     if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2] && squareValues[2] !== '') {
         // do something because a player won on the first row!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5] && squareValues[5] !== '') {
         // do something because a player won on the second row!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8] && squareValues[8] !== '') {
         // do something because a player won on the third row!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6] && squareValues[6] !== '') {
         // do something because a player won on the first column!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7] && squareValues[7] !== '') {
         // do something because a player won on the second column!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8] && squareValues[8] !== '') {
         // do something because a player won on the third column!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8] && squareValues[8] !== '') {
         // do something because a player won on the left-right diagonal!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6] && squareValues[6] !== '') {
         // do something because a player won on the right-left diagonal!
-        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+        gameStatus = `${currentPlayer} is the Winner!!!`
     }
     let boardFull = true;
-    for(let i = 0; i < squareValues.length; i++) {
-        if(squareValues[i] !== "")
+    for (let i = 0; i < squareValues.length; i++) {
+        if(squareValues[i] === "") {
+            boardFull = false;
+        }
     }
-
+    if (boardFull) {
+        gameStatus = `Draw Game!!`
+    }
+    document.getElementById("game-status").innerHTML = gameStatus;
 }
 window.addEventListener("DOMContentLoaded", () => {
     document
         .getElementById("tic-tac-toe-board")
         .addEventListener("click", (event) => {
+            if (gameStatus !== "") return;
             let squareID = event.target.id;
 
             if (!squareID.startsWith('square-')) return;
@@ -71,5 +78,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
     });
 
+    let newGameButton = document.getElementById("new-game");
+    
+    newGameButton.addEventListener("click", (event) => {
+        if (gameStatus === "") return;
+        gameStatus = "";
+        document.getElementById("game-status").innerHTML = gameStatus;
+        squareValues = ["", "", "", "", "", "", "", "", ""];
+        for (let i = 0; i < 9; i++) {
+            document.getElementById(`square-${i}`).innerHTML = "";
+        }
+    })
+    
+
+
+
+    let giveUpButton = document.getElementById("give-up")
 
 });
