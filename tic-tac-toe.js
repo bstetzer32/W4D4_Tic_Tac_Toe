dd76735f07d99f9470318f268fe421f1c35c93bc
Ben Stetzer <benstetzer@gmail.com>
2021-02-04 19:13:50 -0500

    Game Status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,41 @@
 let squareValues = ["", "", "", "", "", "", "", "", ""]
 let currentPlayer = "x"
+let gameStatus = "";
 
+let gameStatusCheck = () => {
+    if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2] && squareValues[2] !== '') {
+        // do something because a player won on the first row!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5] && squareValues[5] !== '') {
+        // do something because a player won on the second row!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8] && squareValues[8] !== '') {
+        // do something because a player won on the third row!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6] && squareValues[6] !== '') {
+        // do something because a player won on the first column!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7] && squareValues[7] !== '') {
+        // do something because a player won on the second column!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8] && squareValues[8] !== '') {
+        // do something because a player won on the third column!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8] && squareValues[8] !== '') {
+        // do something because a player won on the left-right diagonal!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+    if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6] && squareValues[6] !== '') {
+        // do something because a player won on the right-left diagonal!
+        document.getElementById("game-status").innerHTML = `${currentPlayer} is the Winner!!!`
+    }
+}
 window.addEventListener("DOMContentLoaded", () => {
     document
         .getElementById("tic-tac-toe-board")
@@ -23,13 +58,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
             //after each click check to see if it's a win
             //
+            
 
+            gameStatusCheck();
             if(currentPlayer === "x") {
                 currentPlayer = "o";
             } else {
                 currentPlayer = "x";
             }
-
+         
     });
+
 
 });
